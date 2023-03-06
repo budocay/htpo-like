0f9b104663921988eb3d7aab62b0cf5c8e923072
Alexis Lina <alexis.lina@sewan.fr>
2023-03-06 15:42:02 +0100

    feat: add Memory consumption route

--- src/index.mjs
+++ src/index.mjs
@@ -2,42 +2,42 @@ import { h, Component, render } from 'https://unpkg.com/preact?module';
 import htm from 'https://unpkg.com/htm?module';
 
 const html = htm.bind(h);
-function App (props) {
+function AppCPU (propsCPU, propsMemory) {
     return html`
         <div>
-            ${props.cpus.map((cpu) => {
+            <h2>CPU Usage</h2>
+            ${propsCPU.cpus.map((cpu) => {
                return html`
                    <div class="bar">
                        <div class="bar-inner" style="width: ${cpu}%"></div>
                        <label>${cpu.toFixed(2)}%</label>
                    </div>
                `; 
-            })}    
+            })} 
+            <h2>Memory Usage</h2>
+            <div>
+                <div class="bar">
+                </div>
+            </div>
         </div>
     `;
 }
 
-
-/*let update = async () => {
-    let response = await fetch("/api/cpus");
-
-    if (response.status !== 200) {
-        throw new Error(`Http error ! status: ${response.status}`);
-    }
-    let json = await response.json();
-    render(html`<${App} cpus=${json}></${App}>`, document.body);
-
-}
-update()
-setInterval(update, 200);
-*/
-
 let url = new URL("realtime/cpus", window.location.href);
 url.protocol = url.protocol.replace("http", "ws");
-
 let ws = new WebSocket(url.href);
+let url2 = new URL("realtime/memory", window.location.href);
+url2.protocol = url2.protocol.replace("http", "ws");
+let ws2 = new WebSocket(url2.href);
+let jsonCPU;
+let jsonMem;
 ws.onmessage = (ev) => {
-    let json = JSON.parse(ev.data)
-    render(html`<${App} cpus=${json}></${App}>`, document.body);
-
+    ws2.onmessage = (ev2) => {
+        jsonMem = JSON.parse(ev2.data);
+    };
+    jsonCPU = JSON.parse(ev.data);
+    render(html`<${AppCPU} cpus=${jsonCPU} memory=${jsonMem}></${AppCPU}>`, document.body);
 };
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@ use axum::response::{Html, IntoResponse, Response};
 use axum::routing::get;
 use axum::{Json, Router, Server};
 use std::sync::{Arc, Mutex};
+use byte_unit::Byte;
 use sysinfo::{CpuExt, System, SystemExt};
 use tokio::sync::broadcast;
+use serde::{Deserialize, Serialize};
 
 type Snapshot = Vec<f32>;
+type Memshot = Memory;
 
 #[tokio::main]
 async fn main() {
     let (tx, _) = broadcast::channel::<Snapshot>(1);
+    let (tx_mem, _) = broadcast::channel::<Memshot>(1);
 
     let app_state = AppState {
-        tx: tx.clone()
+        tx: tx.clone(),
+        tx_mem: tx_mem.clone()
     };
 
     let router = Router::new()
@@ -22,14 +27,18 @@ async fn main() {
         .route("/index.mjs", get(indexmjs_get))
         .route("/index.css", get(indexcss_get))
         .route("/realtime/cpus", get(realtime_cpus_get))
+        .route("/realtime/memory", get(realtime_memory_get))
         .with_state(app_state.clone());
 
     // Update CPU usage in bg
     tokio::task::spawn_blocking(move || {
         let mut sys = System::new();
         loop {
+            sys.refresh_memory();
             sys.refresh_cpu();
             let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+            let memory = init_memory(&sys);
+            let _ = tx_mem.send(memory);
             let _ = tx.send(v);
             std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
         }
@@ -41,9 +50,37 @@ async fn main() {
     server.await.unwrap();
 }
 
+fn init_memory(sys: &System) -> Memory {
+    Memory {
+        total_mem: bytes_to_gygabites(sys.total_memory() as u128),
+        available_mem:  bytes_to_gygabites(sys.available_memory() as u128),
+        used_mem: bytes_to_gygabites(sys.used_memory() as u128),
+        total_mem_used: bytes_to_gygabites(sys.used_memory() as u128 - sys.available_memory() as u128),
+        total_swap: bytes_to_gygabites(sys.total_swap() as u128),
+        used_swap: bytes_to_gygabites(sys.used_swap() as u128)
+    }
+}
+
+fn bytes_to_gygabites(sys_mem: u128) -> String {
+    let gb_total = Byte::from_bytes(sys_mem);
+    let adjusted_byte = gb_total.get_appropriate_unit(false);
+    adjusted_byte.to_string()
+}
+
 #[derive(Clone)]
 struct AppState {
     tx: broadcast::Sender<Snapshot>,
+    tx_mem: broadcast::Sender<Memshot>
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+struct Memory {
+    total_mem: String,
+    available_mem: String,
+    used_mem: String,
+    total_mem_used: String,
+    total_swap: String,
+    used_swap: String,
 }
 
 #[axum::debug_handler]
@@ -82,10 +119,31 @@ async fn realtime_cpus_get(
     })
 }
 
+
+#[axum::debug_handler]
+async fn realtime_memory_get(
+    ws: WebSocketUpgrade,
+    State(state): State<AppState>,
+) -> impl IntoResponse {
+    ws.on_upgrade(move |ws: WebSocket| async {
+        realtime_memory_stream(state, ws).await;
+    })
+}
+
 async fn realtime_cpus_stream(app_state: AppState, mut ws: WebSocket) {
     let mut rx = app_state.tx.subscribe();
 
     while let Ok(msg) = rx.recv().await {
+        let payload = serde_json::to_string(&msg).unwrap();
+        ws.send(Message::Text(payload)).await.unwrap();
+    }
+}
+
+async fn realtime_memory_stream(app_state: AppState, mut ws: WebSocket) {
+    let mut rx = app_state.tx_mem.subscribe();
+
+    while let Ok(msg) = rx.recv().await {
+
         let payload = serde_json::to_string(&msg).unwrap();
         ws.send(Message::Text(payload)).await.unwrap();
     }
